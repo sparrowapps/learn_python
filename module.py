75a26873af7090774f7bb14d52042135730a1904
sparrowapps <mt.sparrow@gmail.com>
2015-08-20 23:09:12 +0900

    module create and import

---
+++ module.py
@@ -0,0 +1,6 @@
+#module
+
+import mul_class_module
+
+mul2 = mul_class_module.multiplication(2)
+mul2.run()
---
+++ mul_class_module.py
@@ -0,0 +1,46 @@
+#class
+
+#class classname[(parent_class_name)]:
+#   <class variable 1>
+#   <class variable 2>
+
+#   def classfunc1(self[, arg1, arg2,,,]):
+#       <statement 1>
+#       <statement 2>
+
+#   def classfunc2(self[, arg1, arg2,,,]):
+#       <statement 1>
+#       <statement 2>
+
+
+# __init__ (constructor)
+
+# multiplication class
+
+class multiplication:
+    def __init__(self, num):
+        self.num = num
+    def run(self):
+        for j in range (1,10):
+            print(" %d * %d = %d" %(self.num,j, self.num*j))
+
+if __name__ == "__main__":
+    mul2 = multiplication(2)
+    mul3 = multiplication(3)
+    mul4 = multiplication(4)
+    mul5 = multiplication(5)
+    mul6 = multiplication(6)
+    mul7 = multiplication(7)
+    mul8 = multiplication(8)
+    mul9 = multiplication(9)
+
+    mul2.run()
+    mul3.run()
+    mul4.run()
+    mul5.run()
+    mul6.run()
+    mul7.run()
+    mul8.run()
+    mul9.run()
+
+
